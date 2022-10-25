8411204df1d02c295bc9d4cf23e9063c0fb89b38
RafalTaraska <paulo.dorosz@gmail.com>
2022-10-25 11:19:22 +0200

    Init commit

---
+++ lista_zakupow.py
@@ -0,0 +1,11 @@
+lista_zakupow = {
+    "piekarnia": ["Chleb","Paczek","Bulki","Rogal"],
+    "warzywniak": ["Marchew","Seler","Rukola","Piertuszka"]}
+
+v = 0
+for key, value in lista_zakupow.items():
+    print("Ide do",key.upper(),"i kupuje tam",value)
+    ilosc = len(value)
+    v = v + ilosc
+print("Zostały zakupione",v,"towarów")
+print("pierwszy commit")
